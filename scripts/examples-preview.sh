edfe1272a42811b5fc422cd662deec8ddea0bb40
Andrea Stagi <stagi.andrea@gmail.com>
2025-09-30 17:09:03 +0200

    feat: add examples to preview

---
+++ scripts/examples-preview.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ./examples/svelte-app && npm ci && npm run build
+cd ../..
+cd ./examples/react-app && npm ci && npm run build
+cd ../..
+cd ./examples/angular-app && npm ci && npm run build
+cd ../..
+cd ./examples/vue-app && npm ci && npm run build
+cd ../..
+
+cp -R ./examples/svelte-app/dist ./storybook-static/svelte-app/
+cp -R ./examples/vue-app/dist ./storybook-static/vue-app/
+cp -R ./examples/react-app/dist ./storybook-static/react-app/
+cp -R ./examples/angular-app/dist/angular-app/browser ./storybook-static/angular-app/
