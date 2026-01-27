e08d5af1406fa665eb263740e3baceeafc4fc6ae
giuliaghisini <giuliaghisini@users.noreply.github.com>
2026-01-27 11:54:22 +0000

    chore: update documentation [ci skip]

---
+++ assets/it-input-time-emWHQnq1.js
@@ -0,0 +1,31 @@
+import{j as i,M as l,C as o,a as c,D as d}from"./blocks-C-IgPD7H.js";import{useMDXComponents as s}from"./index-UDBjusXW.js";import{S as p,E as t,L as r}from"./it-input-time.stories-CYLh-klh.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-CC_6_ikl.js";import"./types-CucKb5le.js";function a(n){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...n.components};return i.jsxs(i.Fragment,{children:[i.jsx(l,{of:p}),`
+`,i.jsx(e.h1,{id:"input-ora",children:"Input Ora"}),`
+`,i.jsx("description",{children:"Campo per selezionare un orario all'interno di un form"}),`
+`,i.jsx(e.h2,{id:"cosa-fa",children:"Cosa fa"}),`
+`,i.jsx(e.p,{children:`Il componente input orario, o timepicker, consente all'utente di scegliere un orario, indicando ore e minuti.
+È utile per i flussi di interazione che prevedono la comunicazione di un orario, come la prenotazione di appuntamenti o di posti per un evento. Aiuta a prevenire errori, e rende più facile inserire i dati per l'utente.`}),`
+`,i.jsx(e.p,{children:i.jsx(e.a,{href:"https://designers.italia.it/design-system/componenti/timepicker/",rel:"nofollow",children:"Approfondisci come e quando usare il componente Input Ora"})}),`
+`,i.jsx(e.h3,{id:"alternative-a-questo-componente",children:"Alternative a questo componente"}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsx(e.li,{children:"Select (in lavorazione): se hai bisogno che gli utenti selezionino un orario da una lista di opzioni prestabilite, ad esempio con un intervallo di 5 o 10 minuti."}),`
+`]}),`
+`,i.jsx(e.h2,{id:"anteprima-e-attributi-del-componente",children:"Anteprima e attributi del componente"}),`
+`,i.jsxs(e.p,{children:["Modifica gli attributi nella tabella per personalizzare in tempo reale l'aspetto e il comportamento del componente. Per vedere come cambia il codice, clicca su ",i.jsx(e.strong,{children:"Show code"}),"."]}),`
+`,i.jsx(o,{of:t}),`
+`,i.jsx(c,{of:t}),`
+`,i.jsx(e.h2,{id:"indicazioni-generali",children:"Indicazioni generali"}),`
+`,i.jsxs(e.p,{children:["L’input di tipo ora è una variante del componente ",i.jsx(e.code,{children:"<it-input>"})," con l'attributo ",i.jsx(e.code,{children:"type="})," impostato su ",i.jsx(e.code,{children:"time"})," (",i.jsx(e.code,{children:'type="time"'}),")."]}),`
+`,i.jsx(e.p,{children:"Pertanto, per quanto riguarda:"}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsxs(e.li,{children:["la ",i.jsx(e.strong,{children:"validazione"})," e la ",i.jsx(e.strong,{children:"gestione degli errori"})]}),`
+`,i.jsxs(e.li,{children:["la ",i.jsx(e.strong,{children:"gestione degli eventi"})]}),`
+`,i.jsxs(e.li,{children:["i ",i.jsx(e.strong,{children:"metodi e le proprietà"})," accessibili tramite JavaScript"]}),`
+`,i.jsxs(e.li,{children:["il ",i.jsx(e.strong,{children:"supporto all’internazionalizzazione (i18n)"})]}),`
+`]}),`
+`,i.jsxs(e.p,{children:["è necessario fare riferimento alla ",i.jsx(e.a,{href:"?path=/docs/componenti-form-input--documentazione",children:"documentazione di it-input"}),"."]}),`
+`,i.jsx(e.h2,{id:"accessibilità",children:"Accessibilità"}),`
+`,i.jsxs(e.p,{children:["Tutti gli attributi ",i.jsx(e.code,{children:"it-aria-*"})," passati a ",i.jsx(e.code,{children:"<it-input>"})," vengono applicati all'input generato."]}),`
+`,i.jsx(e.h2,{id:"limiti-e-step",children:"Limiti e Step"}),`
+`,i.jsx(d,{of:r}),`
+`,i.jsx(o,{of:r}),`
+`]})}function f(n={}){const{wrapper:e}={...s(),...n.components};return e?i.jsx(e,{...n,children:i.jsx(a,{...n})}):a(n)}export{f as default};
